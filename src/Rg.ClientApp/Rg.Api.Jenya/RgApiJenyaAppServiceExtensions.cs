d0b4d2721819690d041e06cd408b288e402bfde4
Jenya Y. <jenyay@gmail.com>
2015-12-02 13:26:13 +0000

    Add client test app

---
+++ src/Rg.ClientApp/Rg.Api.Jenya/RgApiJenyaAppServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Azure.AppService;
+
+namespace Rg.ClientApp
+{
+    public static class RgApiJenyaAppServiceExtensions
+    {
+        public static RgApiJenya CreateRgApiJenya(this IAppServiceClient client)
+        {
+            return new RgApiJenya(client.CreateHandler());
+        }
+
+        public static RgApiJenya CreateRgApiJenya(this IAppServiceClient client, params DelegatingHandler[] handlers)
+        {
+            return new RgApiJenya(client.CreateHandler(handlers));
+        }
+
+        public static RgApiJenya CreateRgApiJenya(this IAppServiceClient client, Uri uri, params DelegatingHandler[] handlers)
+        {
+            return new RgApiJenya(uri, client.CreateHandler(handlers));
+        }
+
+        public static RgApiJenya CreateRgApiJenya(this IAppServiceClient client, HttpClientHandler rootHandler, params DelegatingHandler[] handlers)
+        {
+            return new RgApiJenya(rootHandler, client.CreateHandler(handlers));
+        }
+    }
+}
