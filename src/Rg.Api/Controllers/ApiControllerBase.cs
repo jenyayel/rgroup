b5b5acd1f1d79dd93d1b3f1482c3a341935bc29a
Jenya Y <jenyay@gmail.com>
2015-12-02 10:41:11 +0000

    initial

---
+++ src/Rg.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,73 @@
+﻿using Rg.ServiceCore.DbModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Security.Claims;
+using Microsoft.Azure.AppService.ApiApps.Service;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Configuration;
+using System;
+
+namespace Rg.Api.Controllers
+{
+    public class ApiControllerBase : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        private Task<Dictionary<string, string>> _apiTokenDictionaryTask;
+        private Task<UserInfo> _userInfoTask;
+        private static HashSet<string> _masterUserEmails;
+
+        protected ApplicationDbContext DbContext
+        {
+            get
+            {
+                return LazyInitializer.EnsureInitialized(
+                    ref _dbContext,
+                    ApplicationDbContext.Create);
+            }
+        }
+
+        protected Task<Dictionary<string, string>> GetApiTokenClaimsAsync()
+        {
+            return LazyInitializer.EnsureInitialized(
+                ref _apiTokenDictionaryTask,
+                async () =>
+                {
+                    var runtime = Runtime.FromAppSettings(Request);
+                    var user = runtime.CurrentUser;
+                    TokenResult token = await user.GetRawTokenAsync("microsoftaccount");
+                    return token.Claims.ToDictionary(c => c.Key, c => c.Value);
+                });
+        }
+
+        protected static HashSet<string> MasterEmails
+        {
+            get
+            {
+                return LazyInitializer.EnsureInitialized(
+                    ref _masterUserEmails,
+                    () => new HashSet<string>(ConfigurationManager.AppSettings["MasterUsers"].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)));
+            }
+        }
+
+        protected Task<UserInfo> GetUserInfoAsync(string userId = null)
+        {
+            if (userId != null)
+            {
+                return DbContext.UserInfos.SingleOrDefaultAsync(u => u.UserInfoId == userId);
+            }
+
+            return LazyInitializer.EnsureInitialized(
+                ref _userInfoTask,
+                async () =>
+                {
+                    string email = (await GetApiTokenClaimsAsync())[ClaimTypes.Email];
+
+                    UserInfo info = await DbContext.UserInfos.SingleOrDefaultAsync(u => u.Email == email);
+                    return info;
+                });
+        }
+    }
+}
---
+++ src/Rg.Api/Controllers/TestController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.AppService.ApiApps.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Rg.Api.Controllers
+{
+    /// <summary>
+    /// Test endpoint to explore API App behavior.
+    /// </summary>
+    public class TestController : ApiController
+    {
+        /// <summary>
+        /// Returns the claims associated with the Microsoft Account token,
+        /// or reports errors if any occur.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IList<string>> Get()
+        {
+            try
+            {
+                var runtime = Runtime.FromAppSettings(Request);
+                if (runtime == null)
+                {
+                    return new[] { "No runtime" };
+                }
+                var user = runtime.CurrentUser;
+                if (user == null)
+                {
+                    return new[] { "No user" };
+                }
+                TokenResult token = await user.GetRawTokenAsync("microsoftaccount");
+                if (token == null)
+                {
+                    return new[] { "No token" };
+                }
+                return token.Claims.Select(c => $"{c.Key}: {c.Value}").ToList();
+            }
+            catch (Exception x)
+            {
+                return new[] { "Exception: " + x };
+            }
+        }
+    }
+}
